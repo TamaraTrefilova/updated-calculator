bcfdefd5312dc3ad5789039b360f9e788c861194
Na Hyung Choi and saswati patra <na@>
2019-06-05 13:38:01 -0700

    updated with subract, multiply and divide

--- js/scripts.js
+++ js/scripts.js
@@ -22,4 +22,25 @@ $(document).ready(function() {
 		var result = add(number1, number2);
 		$("#output").text(result);
 	});
+	$("form#subtract").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#sub1").val());
+		var number2 = parseInt($("#sub2").val());
+		// var result = add(number1, number2);
+		$("#output").text(subtract(number1, number2));
+	});
+	$("form#multiply").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#mul1").val());
+		var number2 = parseInt($("#mul2").val());
+		// var result = add(number1, number2);
+		$("#output").text(multiply(number1, number2));
+	});
+	$("form#divide").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#div1").val());
+		var number2 = parseInt($("#div2").val());
+		// var result = add(number1, number2);
+		$("#output").text(divide(number1, number2));
+	});
 });
